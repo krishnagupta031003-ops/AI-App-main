abf149c15daa80cd5baac56ec33efb2b5d382192
krishnagupta031003-ops <krishnagupta031003>
2026-08-31 12:02:56 +0530

    Deploy latest changes

--- backend/src/config/cors.js
+++ backend/src/config/cors.js
@@ -5,7 +5,7 @@ const corsOptions = {
 
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
 
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: ["Content-Type", "Authorization"], 
 };
 
 module.exports = corsOptions;
